b432d79063a3a77e51987ab0958ba0774f991bcc
Jan Hrůza <hruza.jan1@gmail.com>
2021-04-18 14:01:31 +0200

    Added Pressure plate

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -6,9 +6,9 @@ public class Tile : MonoBehaviour
 {
     public Vector3Int position;
 
-    public enum Interaction{none, tree, destroyable}
+    public enum Interaction { none, tree, destroyable }
 
-    public bool canBuildOn=true;
+    public bool canBuildOn = true;
 
     public Interaction interaction;
     public Grid.TileType type = Grid.TileType.tile;
@@ -18,19 +18,22 @@ public class Tile : MonoBehaviour
         position = new Vector3Int(Mathf.RoundToInt(transform.position.x), Mathf.RoundToInt(transform.position.y), Mathf.RoundToInt(transform.position.z));
     }
 
-    public virtual void TileAction(Vector3Int from){
+    public virtual void TileAction(Vector3Int from)
+    {
 
     }
-    
-    private void Start() {
-        Grid.instance.AddTile(this);    
+
+    private void OnEnable()
+    {
+        Grid.instance.AddTile(this);
     }
     public virtual void PlayerOnPosition(Vector3Int pos)
     {
         return;
     }
 
-    private void OnDisable() {
-        Grid.instance.RemoveTile(this,false);
+    private void OnDisable()
+    {
+        Grid.instance.RemoveTile(this, false);
     }
 }
--- Assets/Tiles/Scripts/TriggerTile.cs
+++ Assets/Tiles/Scripts/TriggerTile.cs
@@ -1,5 +1,5 @@
 using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -7,10 +7,26 @@ public class TriggerTile : Tile
 {
     public UnityEvent onStepTrigger;
 
+    public Transform plate;
+
     public override void PlayerOnPosition(Vector3Int pos)
     {
-        if (pos == position){
+        if (pos == position)
+        {
             onStepTrigger.Invoke();
+            if(plate != null) StartCoroutine(plateDrop()); 
         }
+    }
+
+    IEnumerator plateDrop()
+    {
+        Vector3 pos = plate.position;
+        for (float t = 0; t < 0.5; t += Time.fixedDeltaTime)
+        {
+            plate.position = pos + 0.5f*((t * (0.5f - t)) * Vector3.down);
+            yield return new WaitForFixedUpdate();
+        }
+        plate.position = pos;
+        yield return null;
     }
 }
